f13709cfd8e17db2731ab088671138d9bf4836b4
Muhammad Adib bin Ahmad Roslan <adbroslan@gmail.com>
2017-10-08 02:21:33 +0800

    update commit

--- app/src/main/java/com/hantarkan/hantarkan/MainActivity.java
+++ app/src/main/java/com/hantarkan/hantarkan/MainActivity.java
@@ -3,23 +3,34 @@ package com.hantarkan.hantarkan;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.AdRequest;
+
 import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
     private AdView mAdView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         WebView myWebView = (WebView) findViewById(R.id.webview);
         myWebView.setWebViewClient(new WebViewClient());
 
@@ -40,11 +51,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
-        MobileAds.initialize(this, "ca-app-pub-3745729305407822~2353096875");
-        mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+
 
     }
 
@@ -63,4 +70,35 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public boolean onNavigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.home){
+
+        }else if(id == R.id.uthmm){
+            FragmentManager fm=getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.uthmm,new uthmm()).commit();
+        }else if(id == R.id.uthmf){
+            FragmentManager fm=getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.uthmf,new uthmf()).commit();
+        }else if(id == R.id.pagohm){
+            FragmentManager fm=getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.pagohm,new pagohm()).commit();
+        }else if(id == R.id.pagohf){
+            FragmentManager fm=getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.pagohf,new pagohf()).commit();
+        }
+        return true;
+    }
+
 }
---
+++ app/src/main/java/com/hantarkan/hantarkan/pagohm.java
@@ -0,0 +1,29 @@
+package com.hantarkan.hantarkan;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class pagohm extends Fragment {
+
+
+    public pagohm() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pagohm, container, false);
+    }
+
+}

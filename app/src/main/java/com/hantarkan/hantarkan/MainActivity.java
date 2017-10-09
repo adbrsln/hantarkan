948d83e9d8f750113b1b8b30d68adb4166dfb18c
Muhammad Adib bin Ahmad Roslan <adbroslan@gmail.com>
2017-10-09 14:55:22 +0800

    updated commit

--- app/src/main/java/com/hantarkan/hantarkan/MainActivity.java
+++ app/src/main/java/com/hantarkan/hantarkan/MainActivity.java
@@ -1,9 +1,9 @@
 package com.hantarkan.hantarkan;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -14,16 +14,26 @@ import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
-public class MainActivity extends AppCompatActivity {
+
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private AdView mAdView;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,16 +54,43 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView = (WebView) findViewById(R.id.webview);
         myWebView.setWebViewClient(new WebViewClient());
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        myWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+//        myWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//        myWebView.getSettings().setAppCacheEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+
+
         myWebView.loadUrl("http://hantarkan.com/v2/");
+
+
         myWebView.setWebViewClient(new WebViewClient() {
             @Override
+            public void onPageFinished(WebView view, String url) {
+                findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Context context = getApplicationContext();
+                CharSequence text = "The server or your internet connection may not be active";
+                int duration = Toast.LENGTH_LONG;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+
+            @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
                 if( URLUtil.isNetworkUrl(url) ) {
                     return false;
                 }
@@ -65,24 +102,36 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    public void onPageStarted(WebView view, String url){
 
     }
 
     @Override
     public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setMessage("Are you sure you want to exit?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        MainActivity.super.onBackPressed();
-                    }
-                })
-                .setNegativeButton("No", null)
-                .show();
-
-
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            new AlertDialog.Builder(this)
+                    .setMessage("Are you sure you want to exit?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MainActivity.super.onBackPressed();
+                        }
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -93,24 +142,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
     public boolean onNavigationItemSelected(MenuItem item){
+        Context context = getApplicationContext();
+        CharSequence text = "";
+        int duration = Toast.LENGTH_SHORT;
+
         int id = item.getItemId();
         if(id == R.id.home){
+            myWebView.loadUrl("http://hantarkan.com/v2/");
 
-        }else if(id == R.id.uthmm){
-            FragmentManager fm=getSupportFragmentManager();
-            fm.beginTransaction().replace(R.id.uthmm,new uthmm()).commit();
+         }else if(id == R.id.uthmm){
+            myWebView.loadUrl("http://hantarkan.com/v2/index.php?type=UTHM&gen=m");
+            text = "UTHM Male Drivers";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }else if(id == R.id.uthmf){
-            FragmentManager fm=getSupportFragmentManager();
-            fm.beginTransaction().replace(R.id.uthmf,new uthmf()).commit();
+            myWebView.loadUrl("http://hantarkan.com/v2/index.php?type=UTHM&gen=f");
+            text = "UTHM Female Drivers";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }else if(id == R.id.pagohm){
-            FragmentManager fm=getSupportFragmentManager();
-            fm.beginTransaction().replace(R.id.pagohm,new pagohm()).commit();
+            myWebView.loadUrl("http://hantarkan.com/v2/index.php?type=pagoh&gen=m");
+            text = "Pagoh Male Drivers";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }else if(id == R.id.pagohf){
-            FragmentManager fm=getSupportFragmentManager();
-            fm.beginTransaction().replace(R.id.pagohf,new pagohf()).commit();
+            myWebView.loadUrl("http://hantarkan.com/v2/index.php?type=pagoh&gen=f");
+            text = "Pagoh Female Drivers";
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }else if(id == R.id.about){
+            myWebView.loadUrl("http://hantarkan.com/v2/about.php");
+
         }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
